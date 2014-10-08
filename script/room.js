0f2f81146c04d92849622174d347a5746306b62e
Matt Congel <conge101@mail.chapman.edu>
2014-10-08 14:25:46 -0700

    Testing

--- script/room.js
+++ script/room.js
@@ -503,7 +503,7 @@ var Room = {
 		// Create the stoke button
 		new Button.Button({
 			id: 'stokeButton',
-			text: _("stoke fire"),
+			text: _("fire"),
 			click: Room.stokeFire,
 			cooldown: Room._STOKE_COOLDOWN,
 			width: '80px',
